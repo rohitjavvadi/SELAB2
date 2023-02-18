ac1e93517ab326113ce4821547e0247c1f62bcc3
rohitjavvadi <rohitjavvadi2@gmail.com>
2023-02-18 19:14:10 +0530

    Updated code

--- test.c
+++ test.c
@@ -4,6 +4,8 @@ int main()
    /* printf function displays the content that is
     * passed between the double quotes.
     */
-   printf("Hello World");
+   printf("Hello World")
+
+printf("Updated using branches");
    return 0;
 }
